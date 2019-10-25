a62a90080658b5a7e509f903cf9804f8cbff4a95
Camilo <deivicamiloflorezprieto@gmail.com>
2019-10-25 17:14:12 -0500

    alpha1.4

--- app/src/main/java/com/pixels/colsanbartolome/dialogcarga.java
+++ app/src/main/java/com/pixels/colsanbartolome/dialogcarga.java
@@ -2,6 +2,31 @@ package com.pixels.colsanbartolome;
 
 import android.support.v7.app.*;
 import android.os.*;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class dialogcarga extends AppCompatActivity {
     @Override
---
+++ app/src/main/java/com/pixels/colsanbartolome/profes.java
@@ -0,0 +1,18 @@
+package com.pixels.colsanbartolome;
+
+public class profes
+{
+	private String Usuario,Contraseña,Nombre,Jornada;
+	
+	public profes(){
+		
+	}
+	public profes(String usuario,String contraseña,String nombre,String jornada){
+
+		this.Usuario=usuario;
+		this.Contraseña=contraseña;
+		this.Nombre=nombre;
+		this.Jornada=jornada;
+	}
+	
+}
